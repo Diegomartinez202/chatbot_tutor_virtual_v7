2501bdb32d357d39cecca6fa7e1b064a3204f3e3
Diego Martinez <diegomartin0102@gmail.com>
2025-07-30 20:00:01 -0500

    Actualizacion Docker files backend, rasa , FasApiy docker.

--- docker/init_mongo.js
+++ docker/init_mongo.js
@@ -9,5 +9,5 @@ db.users.insertOne({
     nombre: "Administrador",
     email: "admin@example.com",
     rol: "admin",
-    password: "$2b$12$QZD7zHAGzWYEC2jMsOdqVe2kJxAcS/qPZAzZafk7QoUwNlGNTFxqW" // Hash bcrypt real
+    password: "$2b$12$QZD7zHAGzWYEC2jMsOdqVe2kJxAcS/qPZAzZafk7QoUwNlGNTFxqW" // bcrypt real
 });
